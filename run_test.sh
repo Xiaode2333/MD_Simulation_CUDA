b221092cf711a7d4612300d368916b4dc9268176
Baichuan He <baichuan.he@yale.edu>
2025-11-17 15:45:10 -0500

    fixed fmt print bug

--- run_test.sh
+++ run_test.sh
@@ -6,11 +6,10 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-task=1
 #SBATCH --mem=32G
-#SBATCH --cpu-bind=none
 
 ./set_slurm_env.sh
 
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=$VCPKG_CMAKE -DVCPKG_TARGET_TRIPLET=x64-linux -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DPython3_EXECUTABLE="$(poetry run which python)"
 cmake --build build -j
 
-srun ./build/run_test_run_test
+srun --cpu-bind=none ./build/run_test_run_test
